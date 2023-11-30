933a283ef3261e05a4675351c4bb5db92c724174
Michael <mickaellunzola@gmail.com>
2023-11-30 12:50:18 +0100

    configuration des routes et affichage de produits

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import About from "./pages/About";
+import Products from "./pages/products";
+import ProductsList from "./pages/ProductsList";
+import SingleProduct from "./pages/singleProduct";
+import NavBar from "./components/navBar";
+
 const App = () => {
     return (
         <div>
-            <h1>Hello</h1>
+            <BrowserRouter>
+                <NavBar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/products" element={<Products />}>
+                        <Route index element={<ProductsList />} />
+                        <Route path=":id" element={<SingleProduct />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 };
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,5 +1,11 @@
+import { Outlet } from "react-router-dom";
 const Products = () => {
-    return <h1>Products</h1>;
+    return (
+        <>
+            <h1>Products</h1>
+            <Outlet />
+        </>
+    );
 };
 
 export default Products;
